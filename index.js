2ecfe922f0172662265280facffb8489c392f49e
Jafeth Cohen <jafethdavidcohenhernandez@gmail.com>
2023-10-20 20:17:53 -0500

    segundo commit validar vistas

---
+++ index.js
@@ -0,0 +1,18 @@
+function validacion () {
+
+    var usuario = document.getElementById("username").value;
+    var contraseña = document.getElementById("password").value;
+
+    if (usuario == "admin@gmail.com" && contraseña == "contra123") {
+
+        window.location="vistachill.html"
+        return false
+
+    } if (usuario == "estudiante@gmail.com" && contraseña == "contra1234" ) {
+
+            window.location="vista3.html"
+            return false
+    }
+ 
+
+}
